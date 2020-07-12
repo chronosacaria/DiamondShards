cce4db400697d534b1d5b88844b828ed47b7b8e2
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2020-07-12 13:58:40 -0400

    Removed testing files
    Removed files that were used for testing and cleaned up DiamondShards.java file to remove reference to testing files.

--- src/main/java/com/github/chronosacaria/DiamondShards.java
+++ src/main/java/com/github/chronosacaria/DiamondShards.java
@@ -7,13 +7,11 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class DiamondShards implements ModInitializer {
-	private static final Item DIAMOND_SHARD = new DiamondShardItem(new Item.Settings().group(ItemGroup.MATERIALS));
+	private static final Item DIAMOND_SHARD = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("diamondshards", "diamond_shard"), DIAMOND_SHARD);
-
-		System.out.println("Hello Fabric world!");
 
 	}
 }
